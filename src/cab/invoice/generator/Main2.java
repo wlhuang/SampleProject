0bda72aa73aaca32894ee8464babf242bec73edb
wlhuang <celine.hwl@gmail.com>
2014-04-29 10:58:25 +0800

    Create Main2.java

---
+++ src/cab/invoice/generator/Main2.java
@@ -0,0 +1,13 @@
+package cab.invoice.generator;
+
+public class Main2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Another main class for demonstration only");
+
+	}
+
+}
